2282067d20ee7b8bd9acd927c85053b19031f5e0
mr-ryan12 <mr.ryan444@gmail.com>
2021-09-25 00:43:02 -0600

    Pass unicorn tests

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -2,6 +2,19 @@ class Unicorn {
   constructor(name, color) {
     this.name = name;
     this.color = color;
+    if (color === undefined) {
+      this.color = 'white';
+    }
+  }
+
+  isWhite() {
+    if (this.color != 'white') {
+      return false;
+    }
+  }
+
+  says(string) {
+    return `**;* ${string} *;**`;
   }
 }
 
