aa8fda2f09102bbc6c296e2c50c64b484fb855ff
Ryan McBride <mr.ryan444@gmail.com>
2021-09-27 23:04:39 -0600

    Pass medusa tests

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,22 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  };
+
+  gazeAtVictim(victim) {
+    var statue = new Statue(victim.name);
+    this.statues.push(statue);
+    if (this.statues.length > 3) {
+      var person = this.statues.splice(0, 1);
+      var freedStatue = new Person(person[0].name);
+      freedStatue.mood = 'relieved';
+      return freedStatue;
+    };
+  };
+};
+
+module.exports = Medusa;
