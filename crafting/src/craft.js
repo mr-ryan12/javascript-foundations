67ca95f62761068a8ac47e3bdca8fd81b1436d1a
Ryan McBride <mr.ryan444@gmail.com>
2021-10-18 20:00:06 -0600

    Pass craft tests

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,22 @@
 class Craft {
+  constructor(craftObject) {
+    this.name = craftObject.type;
+    this.materials = craftObject.materials;
+    this.completed = false;
+  }
 
+  completeCraft() {
+    this.completed = true;
+    return 'All done! It looks great!';
+  }
+
+  calculateProjectTotal() {
+    var canvasPrice = this.materials[0].num1 * this.materials[0].num2;
+    var paintPrice = this.materials[1].num1 * this.materials[1].num2;
+    var total = canvasPrice + paintPrice;
+
+    return total;
+  }
 }
 
 module.exports = Craft;
--- crafting/src/material.js
+++ crafting/src/material.js
@@ -1,5 +1,11 @@
 class Material {
-
+  constructor(canvas, num1, num2, sheets) {
+    this.canvas = canvas;
+    this.num1 = num1;
+    this.num2 = num2;
+    this.sheets = sheets;
+  }
 }
+
 
 module.exports = Material;
